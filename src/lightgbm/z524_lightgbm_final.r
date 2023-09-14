1b0b2a6bec2a5547ead5ad821cdd9cb86d70aee1
GustavoDenicolay <gustavo.denicolay@gmail.com>
2023-09-14 14:17:28 -0300

    z524_lightgbm_final.r

--- src/lightgbm/z524_lightgbm_final.r
+++ src/lightgbm/z524_lightgbm_final.r
@@ -24,11 +24,11 @@ PARAM$input$future <- c(202107) # meses donde se aplica el modelo
 
 PARAM$finalmodel$semilla <- 100005
 
-PARAM$finalmodel$num_iterations <- 670
-PARAM$finalmodel$learning_rate <- 0.0100738668153211
-PARAM$finalmodel$feature_fraction <- 0.413040235996875
-PARAM$finalmodel$min_data_in_leaf <- 2858
-PARAM$finalmodel$num_leaves <- 161
+PARAM$finalmodel$num_iterations <- 559
+PARAM$finalmodel$learning_rate <- 0.0100746999
+PARAM$finalmodel$feature_fraction <- 0.5144127527
+PARAM$finalmodel$min_data_in_leaf <- 505
+PARAM$finalmodel$num_leaves <- 44
 
 
 PARAM$finalmodel$max_bin <- 31
