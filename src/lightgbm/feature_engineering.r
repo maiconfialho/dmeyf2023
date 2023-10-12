dd3ff55e54e5c11bcb3ec47ce0e1e2e698db5d0e
maiconfialho <maicon.fialho@ufv.br>
2023-10-12 03:04:03 -0300

    feature eng

--- src/lightgbm/feature_engineering.r
+++ src/lightgbm/feature_engineering.r
@@ -4,13 +4,13 @@ gc() # garbage collection
 require("data.table")
 library(dplyr)
 
-
+#setwd("/Users/maiconfialho/Documents/Mestrado/2023-2/dmeyf2023/") # Establezco el Directorio de Trabajo
 setwd("C:/Users/maico/Documents/Mestrado/dmeyf2023/") # Establezco el Directorio de Trabajo
 dataset <- fread("./datasets/competencia_02.csv.gz")
 dataset <- dataset[order(numero_de_cliente, foto_mes)]
 
-dataset <- dataset %>%
-  mutate_all(funs(lag = lag(.)))
+#dataset <- dataset %>%
+ # mutate_all(funs(lag = lag(.)))
 
 # Total de deudas
 colunas_deudas <- c("mprestamos_personales", "mprestamos_prendarios", "mprestamos_hipotecarios", "Visa_msaldototal", "Master_msaldototal")
@@ -50,23 +50,23 @@ dataset[, saldo_pos := ifelse(mcaja_ahorro + mcuenta_corriente > 0, 1, 0)]
 # Cuánto tiempo la mcuenta_corriente no aumenta el saldo
 dataset[, saldo_anterior_corriente := shift(mcuenta_corriente, n = -1, fill = 0), by = numero_de_cliente]
 dataset[, aumento_saldo_corriente := mcuenta_corriente > saldo_anterior_corriente]
-dataset[, tiempo_sin_aumento := cumsum(!aumento_saldo_corriente), by = numero_de_cliente]
+dataset[, tiempo_sin_aumento_cc := cumsum(!aumento_saldo_corriente), by = numero_de_cliente]
 dataset[aumento_saldo_corriente == TRUE, tiempo_sin_aumento := 0, by = numero_de_cliente]
 
 # Cuánto tiempo la mcaja_ahorro no aumenta el saldo
 dataset[, saldo_anterior_ahorro := shift(mcaja_ahorro, n = -1, fill = 0), by = numero_de_cliente]
 dataset[, aumento_saldo_ahorro := mcaja_ahorro > saldo_anterior_ahorro]
-dataset[, tiempo_sin_aumento := cumsum(!aumento_saldo_ahorro), by = numero_de_cliente]
+dataset[, tiempo_sin_aumento_ah := cumsum(!aumento_saldo_ahorro), by = numero_de_cliente]
 dataset[aumento_saldo_ahorro == TRUE, tiempo_sin_aumento := 0, by = numero_de_cliente]
 
 # Cuánto tiempo la mcuenta_corriente no disminuye el saldo
 dataset[, disminuye_saldo_corriente := mcuenta_corriente < saldo_anterior_corriente]
-dataset[, tiempo_sin_disminuir := cumsum(!disminuye_saldo_corriente), by = numero_de_cliente]
+dataset[, tiempo_sin_disminuir_cc := cumsum(!disminuye_saldo_corriente), by = numero_de_cliente]
 dataset[disminuye_saldo_corriente == TRUE, tiempo_sin_disminuir := 0, by = numero_de_cliente]
 
 # Cuánto tiempo la mcaja_ahorro no disminuye el saldo
 dataset[, disminuye_saldo_ahorro := mcaja_ahorro < saldo_anterior_ahorro]
-dataset[, tiempo_sin_disminuir := cumsum(!disminuye_saldo_ahorro), by = numero_de_cliente]
+dataset[, tiempo_sin_disminuir_ah := cumsum(!disminuye_saldo_ahorro), by = numero_de_cliente]
 dataset[disminuye_saldo_ahorro == TRUE, tiempo_sin_disminuir := 0, by = numero_de_cliente]
 
 # Cantidad total de tarjetas de crédito
@@ -114,10 +114,13 @@ dataset[, paro_de_recibir_sueldo := shift(recibe_sueldo, n = -1, type = "lead", 
 dataset[, paro_de_recibir_sueldo := (recibe_sueldo < paro_de_recibir_sueldo)]
 
 # Cantidad total de débitos automáticos
-dataset[, qtd_debito_automatico := ccuenta_debitos_automaticos + tarjeta_visa_debitos_automaticos + ctarjeta_master_debitos_automaticos]
+dataset[, qtd_debito_automatico := ccuenta_debitos_automaticos + ctarjeta_visa_debitos_automaticos + ctarjeta_master_debitos_automaticos]
 
 # Tiene débito automático
 dataset[, tiene_debito_automatico := ifelse(qtd_debito_automatico > 0, 1, 0)]
+
+# cantidade de debitos automaticos ultimo mes
+dataset[, qtd_debito_automatico_ult_mes := shift(qtd_debito_automatico, n = -1, type = "lead", fill = 0), by = numero_de_cliente]
 
 # Cantidad de débitos automáticos disminuyó
 dataset[, disminuyo_debito_automatico := (qtd_debito_automatico < qtd_debito_automatico_ult_mes)]
@@ -128,12 +131,18 @@ dataset[, aumento_debito_automatico := (qtd_debito_automatico > qtd_debito_autom
 # Pago mis cuentas
 dataset[, pago_mis_cuentas := ifelse(dataset$cpagomiscuentas > 0, 1, 0)]
 
+# Pago mis cuentas ultimo mes
+dataset[, pago_mis_cuentas_ult_mes := shift(pago_mis_cuentas, n = -1, type = "lead", fill = 0), by = numero_de_cliente]
+
 # Cantidad de pago mis cuentas disminuyó o aumentó
 dataset[, disminuyo_pago_mis_cuentas := (pago_mis_cuentas < pago_mis_cuentas_ult_mes)]
 dataset[, aumento_pago_mis_cuentas := (pago_mis_cuentas > pago_mis_cuentas_ult_mes)]
 
 # Pago de servicios
 dataset[, pago_servicios := ifelse(dataset$cpagodeservicios > 0, 1, 0)]
+
+# Pago de servicios ultimo mes
+dataset[, pago_servicios_ult_mes := shift(pago_servicios, n = -1, type = "lead", fill = 0), by = numero_de_cliente]
 
 # Cantidad de pago de servicios aumentó o disminuyó
 dataset[, disminuyo_pago_servicios := (pago_servicios < pago_servicios_ult_mes)]
@@ -142,11 +151,18 @@ dataset[, aumento_pago_servicios := (pago_servicios > pago_servicios_ult_mes)]
 # Hace transferencias
 dataset[, hace_transferencias := ifelse(dataset$mtransferencias_emitidas > 0, 1, 0)]
 
+# transferencias emitidas ultimo mes
+dataset[, transferencias_emitidas_ult_mes := shift(mtransferencias_emitidas, n = -1, type = "lead", fill = 0), by = numero_de_cliente]
+
 # Recibe transferencias
 dataset[, recibe_transferencias := ifelse(dataset$mtransferencias_recibidas > 0, 1, 0)]
 
+# transferencias recibidas ultimo mes
+dataset[, transferencias_recibidas_ult_mes := shift(mtransferencias_recibidas, n = -1, type = "lead", fill = 0), by = numero_de_cliente]
+
 # Balance de transferencias
 dataset[, balance_transferencias := mtransferencias_recibidas - mtransferencias_emitidas]
+
 
 # Cantidad de transferencias emitidas disminuyó o aumentó
 dataset[, disminuyo_transferencias_emitidas := (mtransferencias_emitidas < transferencias_emitidas_ult_mes)]
@@ -186,14 +202,15 @@ columnas_para_eliminar <- c("ultimo_minversion1_pesos", "ultimo_minversion1_dola
                             "saldo_anterior_ahorro", "saldo_anterior_corriente", "total_tarjetas_ult_mes",
                             "uso_tar_debito_ult_mes", "uso_tar_visa_ult_mes", "uso_tar_master_ult_mes",
                             "uso_seguro_ult_mes", "paro_de_recibir_sueldo", "media_3_meses_saque",
-                            "media_3_meses_cheques_depositados", "media_3_meses_cheques_emitidos"
-                        )
+                            "media_3_meses_cheques_depositados", "media_3_meses_cheques_emitidos",
+                            "qtd_debito_automatico_ult_mes", "qtd_debito_automatico_ult_mes",
+                            "pago_mis_cuentas_ult_mes", "pago_servicios_ult_mes", 
+                            "transferencias_emitidas_ult_mes", "transferencias_recibidas_ult_mes")
 
 # Eliminar las columnas especificadas del conjunto de datos
-dataset_limpio <- dataset[, !columnas_para_eliminar, with = FALSE]
-
-
-na_count <- rowSums(is.na(dataset))
+#dataset <- dataset[, !columnas_para_eliminar, with = FALSE]
+#dataset <- dataset[, !(names(dataset) %in% columnas_para_eliminar)]
+dataset <- dataset %>% select(-one_of(columnas_para_eliminar))
 
 infinitos <- lapply(names(dataset),function(.name) dataset[ , sum(is.infinite(get(.name)))])
 infinitos_qty <- sum(unlist(infinitos))
@@ -201,12 +218,7 @@ infinitos_qty <- sum(unlist(infinitos))
 nans <- lapply(names(dataset),function(.name) dataset[ , sum(is.nan(get(.name)))])
 nans_qty <- sum(unlist(nans))
 
-fwrite(dataset, 1000,
-        "./datasets/competencia_02_fe_lag_bruto.csv",
-        logical01= TRUE,
-        sep= "," )
-
-fwrite(dataset_limpio,
-        "./datasets/competencia_02_fe_lag_limpio.csv",
+fwrite(dataset,
+        "./datasets/competencia_02_fe.csv",
         logical01= TRUE,
         sep= "," )
