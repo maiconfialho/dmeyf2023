ef55cc88f5cd567669642cfb87a3486a463eca31
MaiconFialho <maicon.fialho@ufv.br>
2023-10-13 02:31:14 -0300

    preparo para video

---
+++ video_miranda/dados para apresentacao.rb
@@ -0,0 +1,61 @@
+
+Resumo grupos:
+**Grupo 0:**
+Clientes ativos no Home Banking que realizam muitas transações por esse canal, incluindo débitos automáticos na tarjeta de crédito VISA e transferências. Não são focados em Acreditações de Haberes.
+
+**Grupo 1:**
+Clientes com alta atividade no Home Banking e contas ativas na tarjeta de crédito VISA. Realizam transferências substanciais e frequentemente têm algum tipo de acordo de descoberto.
+
+**Grupo 2:**
+Clientes com dívidas de empréstimos pessoais, envolvidos em transações por home banking, transferências e uso frequente de serviços de call center.
+
+**Grupo 3:**
+Clientes com dívidas de empréstimos pessoais, uso frequente de internet e um histórico de transferências significativas. Possuem contas VISA.
+
+**Grupo 4:**
+Clientes com dívidas de empréstimos pessoais e transferências, com um limite de financiamento em suas contas de cartão de crédito Master. Podem possuir contas VISA.
+
+**Grupo 5:**
+Clientes que recebem várias Acreditações de Haberes em relação de dependência e pagamento de empregadores acreditados. Mantêm um saldo mais alto em suas contas de caixa de poupança do Paquete Premium, usam menos a tarjeta de crédito VISA e cumprem com os pagamentos mínimos.
+
+**Grupo 6:**
+Clientes ativos no Home Banking que realizam muitas transações por esse canal, incluindo transferências. Possuem contas VISA e frequentemente estão registrados no Home Banking.
+
+Grupos semelhantes:
+
+1. **Grupo 0 e Grupo 6:** Novo grupo 0
+   - Ambos os grupos são caracterizados por clientes ativos no Home Banking e envolvidos em transações por esse canal. Eles podem ser agrupados como "Clientes Ativos no Home Banking."
+
+2. **Grupo 1 e Grupo 4:** Novo grupo 1
+   - Esses grupos têm características semelhantes, incluindo altas transações por home banking e possuem contas de cartão de crédito (VISA ou Master). Eles podem ser agrupados como "Clientes com Atividade Bancária Online e Contas de Crédito."
+
+3. **Grupo 2 e Grupo 3:** Novo grupo 2
+   - Ambos os grupos têm dívidas de empréstimos pessoais e estão envolvidos em transferências. Eles podem ser agrupados como "Clientes com Empréstimos Pessoais e Atividade Bancária Online."
+
+4. **Grupo 5:** Novo grupo 3
+   - Este grupo é caracterizado por clientes que recebem Acreditações de Haberes e têm saldos mais elevados em suas contas de poupança. Pode ser mantido como um grupo separado de "Clientes com Receita Estável e Saldo Alto."
+
+O Grupo 2 possui a maior média de mcomisiones e mrentabilidad, sugerindo que esse grupo gera uma quantidade significativa de comissões e lucros para o banco.
+
+O Grupo 0 também tem médias substanciais para mcomisiones e mrentabilidad, indicando que este grupo contribui significativamente para o banco em termos de comissões e lucros.
+
+Os Grupos 1 e 3 têm médias menores para ambas as variáveis, sugerindo que eles geram comissões e lucros mais baixos em comparação com os grupos 0 e 2.
+
+Grupo 0:
+
+Média de mcomisiones: 706.85
+Média de mrentabilidad: 1777.86
+Grupo 1:
+
+Média de mcomisiones: 695.63
+Média de mrentabilidad: 1332.98
+Grupo 2:
+
+Média de mcomisiones: 963.62
+Média de mrentabilidad: 2387.05
+Grupo 3:
+
+Média de mcomisiones: 499.82
+Média de mrentabilidad: 1722.16
+
+utilizando essas informacoes gere um script para uma apresentacao para a cmo miranda do banco dmeyf, onde sera apresetentando os grupos que foram identificados com base em um algoritmo de ciencia de dados, como esses grupos puderam ser resumidos, oq cada grupo representa, ants e depois da uniao dos grupos semelhantes e uma explicacao dos dados coletados ferentes a comissoes e rentabilidade de cada grupo. esse script deve sser para uma apresentacao de no maximo 5 minutos e minimo de 3
